a3eb16e27390c114c8505e90bc9767d74183e42c
Bilal <azbk2004@gmail.com>
2023-05-22 00:21:34 +0600

    ~

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,7 +1,12 @@
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+path = '/home/Bilal312/AtomzCoffee'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
 application = get_wsgi_application()
